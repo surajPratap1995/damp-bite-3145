b37324dc32015ff331fb5fb9407837b4fd744c45
Suraj Pratap <advocatesurajpratap95@gmail.com>
2022-11-11 22:49:30 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import Navbar from  "./Navbar/Navbar"
+import {Routes,Route} from "react-router-dom"
+import Home from "./Route/Home"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
     </div>
   );
 }
